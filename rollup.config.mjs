58959d479a07b0ea810d1f58a5a5e785dadd0d36
IceOfSummer <xu2237803016@outlook.com>
2024-10-28 15:53:03 +0800

    feat: 打包所有依赖

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,15 @@
 // rollup.config.mjs
 import typescript from '@rollup/plugin-typescript';
+import nodeResolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import terser from "@rollup/plugin-terser"
 
 export default {
   input: 'src/main.ts',
   output: {
     file: 'dest/bundle.js',
-    format: 'es'
+    format: 'es',
+    compact: true,
   },
-  plugins: [typescript()]
+  plugins: [typescript(),commonjs(), nodeResolve(), terser()]
 }
